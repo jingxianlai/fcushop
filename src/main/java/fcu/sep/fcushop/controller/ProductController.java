b5be39e09bf88d29b0b92e8d8fe0adedf7987c7a
D0813188 <laijingxianruby@gmail.com>
2022-01-10 14:08:27 +0800

    update+checkstyle

--- src/main/java/fcu/sep/fcushop/controller/ProductController.java
+++ src/main/java/fcu/sep/fcushop/controller/ProductController.java
@@ -2,10 +2,20 @@ package fcu.sep.fcushop.controller;
 
 import fcu.sep.fcushop.model.Product;
 import fcu.sep.fcushop.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * this is a class of product controller.
+ */
+
 
 @RestController
 public class ProductController {
@@ -23,6 +33,7 @@ public class ProductController {
   public List<Product> getProducts(@PathVariable("keyword") String keyword) {
     return productManager.getProducts(keyword);
   }
+
   @RequestMapping(value = "/addproduct", method = RequestMethod.GET)
   @ResponseBody
   public String addProduct(
@@ -37,7 +48,7 @@ public class ProductController {
   @RequestMapping(value = "/updateproduct", method = RequestMethod.GET)
   @ResponseBody
   public String updateProduct(
-      @RequestParam("bookname") String name,
+      @RequestParam("name") String name,
       @RequestParam("price") Integer price
   ) {
     return productManager.aaUpdateProduct(name, price);
--- src/main/java/fcu/sep/fcushop/service/ProductService.java
+++ src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -2,11 +2,14 @@ package fcu.sep.fcushop.service;
 
 import fcu.sep.fcushop.database.Sql2oDbHandler;
 import fcu.sep.fcushop.model.Product;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
 
-import java.util.List;
+/**
+ * this is a class of product service.
+ */
 
 @Service
 public class ProductService {
@@ -18,18 +21,28 @@ public class ProductService {
 
   }
 
+  /**
+   * this is a class of product service.
+   */
+
   public List<Product> getProducts() {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl, "
+          + "PRICE price, DESCRIPTION description"
           + " from PRODUCT";
 
       return connection.createQuery(query).executeAndFetch(Product.class);
     }
   }
 
+  /**
+   * this is a class of product service.
+   */
+
   public List<Product> getProducts(String keyword) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl,"
+          + " PRICE price, DESCRIPTION description"
           + " from PRODUCT where name like :keyword";
 
       return connection.createQuery(query)
@@ -37,6 +50,10 @@ public class ProductService {
           .executeAndFetch(Product.class);
     }
   }
+
+  /**
+   * this is a class of product service.
+   */
 
   public String aaAddProduct(String name, String imgurl, int price,
                               String description) {
@@ -63,7 +80,7 @@ public class ProductService {
   public String aaUpdateProduct(String name, int price) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "Update product "
-          + "SET PRICE= :price WHERE NAME = :book_name";
+          + "SET PRICE= :price WHERE NAME = :name";
 
       System.out.println(query);
       connection.createQuery(query)
